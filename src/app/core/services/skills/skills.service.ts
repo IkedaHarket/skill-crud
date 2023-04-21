c246a3be6e8c4c1751fc7b597af5839e0796a904
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2023-04-21 19:00:12 -0400

    crud listo

--- src/app/core/services/skills/skills.service.ts
+++ src/app/core/services/skills/skills.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-
 import { v4 as uuidv4 } from 'uuid';
-import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
-import { CreateSkillDTO, Skill } from '../../interfaces';
-import { environment } from 'src/environments/environment';
+import { BehaviorSubject} from 'rxjs';
+
 import { AlertService } from 'src/app/core/services';
+import { CreateSkillDTO, Skill } from '../../interfaces';
 import SkillsData from "../skills-data";
 
 @Injectable({
@@ -17,42 +15,42 @@ export class SkillsService {
   public skills$ = this._skills.asObservable();
 
   constructor(
-    private readonly http: HttpClient,
     private readonly alert: AlertService,
-  ) { }
+  ) { 
 
-  getSkills(): Observable<Skill[]>{
-    return this.http.get<Skill[]>(`${environment.backendUrl}skills`).pipe(
-      tap((resp) => this._skills.next(resp)),
-      catchError((e) => this.handleErrorApi(e))
-    )
+    this.getSkills();
+  }
+
+  getSkills(){
+    const skills = this.readLocalStorage();
+    if(skills.length <= 0 ) return;
+    this._skills.next(skills);
   }
   
   postSkill(skill:CreateSkillDTO){
       this.alert.displayToast({icon:'success',title:'Habilidad creada'})
       this._skills.next([...<[]>this._skills.value, {...skill, id: uuidv4()}])
+      this.writeLocalStorage(this._skills.value!)
   }
 
   editSkill(newSkill:CreateSkillDTO, idSkill:string){
       this.alert.displayToast({icon:'success',title:'Habilidad editada'})
       const newSkills = this._skills.value?.map((skill) => (skill.id === idSkill)? {...newSkill, id: idSkill}: skill ) || []
       this._skills.next(newSkills)
+      this.writeLocalStorage(newSkills)
   }
   
   deleteSkill(idSkill:string){
     this.alert.displayToast({icon:'success',title:'Habilidad borrada'})
     this._skills.next([...<[]>this._skills.value?.filter(({id}) => id !== idSkill)])
+    this.writeLocalStorage(this._skills.value!)
   }
 
-  private handleErrorApi(error : HttpErrorResponse, crudType?: 'agregar' | 'eliminar' | 'editar' ){
-
-    if(crudType){
-      this.alert.displayToast({icon: 'error', title: `Error al ${crudType} la habilidad`})
-    }
-
-    return throwError(
-      () => new Error(`Un error ha ocurrido; por favor reintente mas tarde.`)
-    );
+  private readLocalStorage(): Skill[]{
+    return JSON.parse(localStorage.getItem('skills') || '[]') || []
   }
 
+  private writeLocalStorage(skills: Skill[]): void{
+    localStorage.setItem('skills', JSON.stringify(skills))
+  }
 }
